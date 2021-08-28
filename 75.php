fae6f015c66101a28a787f44328880938d9cc601
danoxv <65130664+danoxv@users.noreply.github.com>
2021-08-28 19:39:17 +0300

    removed a piece of code for further work

--- 75.php
+++ 75.php
@@ -8,12 +8,3 @@ class Hidden extends Input {
         parent::__construct();
     }
 }
-
-$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'GET']);
-
-echo $form->open();
-echo (new Hidden)->setAttr('name', 'id')->setAttr('value', '123');
-echo (new Input)->setAttr('name', 'year');
-echo new Submit;
-
-echo $form->close();
